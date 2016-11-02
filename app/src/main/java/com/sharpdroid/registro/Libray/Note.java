ac7e524ccc7767c520070d2a47e1c767c292b97d
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-11-02 17:51:14 +0100

    Add Note interface

---
+++ app/src/main/java/com/sharpdroid/registro/Libray/Note.java
@@ -0,0 +1,49 @@
+package com.sharpdroid.registro.Libray;
+
+import java.io.Serializable;
+
+public class Note implements Serializable {
+    private String teacher;
+    private String date;
+    private String content;
+    private String type;
+
+    public Note(String teacher, String date, String content, String type) {
+        this.teacher = teacher;
+        this.date = date;
+        this.content = content;
+        this.type = type;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
