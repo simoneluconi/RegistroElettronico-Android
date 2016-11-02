7be3e052a06c1d59f7f860c33f8c800af3c75447
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-11-02 12:55:07 +0100

    Finish the API

--- app/src/main/java/com/sharpdroid/registro/RESTFulAPI.java
+++ app/src/main/java/com/sharpdroid/registro/RESTFulAPI.java
@@ -7,7 +7,31 @@ class RESTFulAPI {
 
     final String MARKS_URL = BASE_URL + "marks";
 
+    final String FILES_URL = BASE_URL + "file";
+
+    final String ABSANCES_URL = BASE_URL + "absences";
+
+    final String SUBJECTS_URL = BASE_URL + "subjects";
+
+    final String NOTES_URL = BASE_URL + "notes";
+
+    final String COMMUNICATIONS_URL = BASE_URL + "communications";
+
+    final String SCRUTINIES_URL = BASE_URL + "scrutinies";
+
     RESTFulAPI() {
 
+    }
+
+    public String FILE_DOWNLOAD_URL(String id, String cksum) {
+        return String.format("%s/%s/%s/download", FILES_URL, id, cksum);
+    }
+
+    public String SUBJECT_LESSONS_URL(String id) {
+        return String.format("%s/%s/lessons", SUBJECTS_URL, id);
+    }
+
+    public String COMMUNICATION_URL(String id) {
+        return String.format("%s/%s/desc", COMMUNICATIONS_URL, id);
     }
 }
