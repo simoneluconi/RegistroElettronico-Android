aa043f706a2fb4cfef320e02cfe48babb99b2f1a
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-11-02 17:53:05 +0100

    Add Communication interfaces

---
+++ app/src/main/java/com/sharpdroid/registro/Libray/Communication.java
@@ -0,0 +1,49 @@
+package com.sharpdroid.registro.Libray;
+
+import java.io.Serializable;
+
+public class Communication implements Serializable {
+    private String title;
+    private String type;
+    private String date;
+    private int id;
+
+    public Communication(String title, String type, String date, int id) {
+        this.title = title;
+        this.type = type;
+        this.date = date;
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
---
+++ app/src/main/java/com/sharpdroid/registro/Libray/CommunicationDescription.java
@@ -0,0 +1,39 @@
+package com.sharpdroid.registro.Libray;
+
+import java.io.Serializable;
+
+public class CommunicationDescription implements Serializable {
+    private String longTitle;
+    private String desc;
+    private boolean attachment;
+
+    public CommunicationDescription(String longTitle, String desc, boolean attachment) {
+        this.longTitle = longTitle;
+        this.desc = desc;
+        this.attachment = attachment;
+    }
+
+    public String getLongTitle() {
+        return longTitle;
+    }
+
+    public void setLongTitle(String longTitle) {
+        this.longTitle = longTitle;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public boolean isAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(boolean attachment) {
+        this.attachment = attachment;
+    }
+}
