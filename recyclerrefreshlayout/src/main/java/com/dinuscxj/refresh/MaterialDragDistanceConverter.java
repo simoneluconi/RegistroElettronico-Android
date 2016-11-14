6c040a01e8f7d70beea8bb7d89aef0dc24b6c4df
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-11-14 22:45:37 +0100

    Import recyclerrefreshlayout
    * Also do some code/style fixes of the library

---
+++ recyclerrefreshlayout/src/main/java/com/dinuscxj/refresh/MaterialDragDistanceConverter.java
@@ -0,0 +1,16 @@
+package com.dinuscxj.refresh;
+
+class MaterialDragDistanceConverter implements IDragDistanceConverter {
+    @Override
+    public float convert(float scrollDistance, float refreshDistance) {
+        float originalDragPercent = scrollDistance / refreshDistance;
+        float dragPercent = Math.min(1.0f, Math.abs(originalDragPercent));
+        float extraOS = Math.abs(scrollDistance) - refreshDistance;
+        float tensionSlingshotPercent = Math.max(0, Math.min(extraOS, refreshDistance * 2.0f) / refreshDistance);
+        float tensionPercent = (float) ((tensionSlingshotPercent / 4) -
+                Math.pow((tensionSlingshotPercent / 4), 2)) * 2f;
+        float extraMove = (refreshDistance) * tensionPercent * 2;
+
+        return (int) ((refreshDistance * dragPercent) + extraMove);
+    }
+}
