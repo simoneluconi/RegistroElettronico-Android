67050eba6c79458fa082aea08611a879f8822e20
Marco <bortolanmarco@gmail.com>
2016-11-16 17:39:42 +0100

    Absences Methods & layouts

--- app/src/main/java/com/sharpdroid/registro/Interfaces/Absences.java
+++ app/src/main/java/com/sharpdroid/registro/Interfaces/Absences.java
@@ -14,6 +14,17 @@ public class Absences implements Serializable {
         this.exits = exits;
     }
 
+    public void setData(Absences a){
+        absences.addAll(a.getAbsences());
+        delays.addAll(a.getDelays());
+        exits.addAll(a.getExits());
+    }
+    public void clear(){
+        absences.clear();
+        delays.clear();
+        exits.clear();
+    }
+
     public List<Absence> getAbsences() {
         return absences;
     }
@@ -36,5 +47,38 @@ public class Absences implements Serializable {
 
     public void setExits(List<Exit> exits) {
         this.exits = exits;
+    }
+
+    /**
+     * @param i 0: absences 1: delays 2: exits
+     * @return Size of list
+     */
+    public int getSize(int i) {
+        switch (i) {
+            case 0:
+                return absences.size();
+            case 1:
+                return delays.size();
+            case 2:
+                return exits.size();
+        }
+        return 0;
+    }
+
+    /**
+     * @param i 0: absences 1: delays 2: exits
+     * @return Data of list
+     */
+    public List getGroup(int i) {
+        switch (i) {
+            case 0:
+                return absences;
+            case 1:
+                return delays;
+            case 2:
+                return exits;
+            default:
+                return null;
+        }
     }
 }
