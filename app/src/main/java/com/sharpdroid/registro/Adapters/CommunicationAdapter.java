edd35b1fbb880a4e9c4862ab02aba2b0bdf67651
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-11-17 23:21:07 +0100

    Download all the stuff

--- app/src/main/java/com/sharpdroid/registro/Adapters/CommunicationAdapter.java
+++ app/src/main/java/com/sharpdroid/registro/Adapters/CommunicationAdapter.java
@@ -1,14 +1,24 @@
 package com.sharpdroid.registro.Adapters;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Environment;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.koushikdutta.ion.Ion;
+import com.sharpdroid.registro.API.RESTFulAPI;
 import com.sharpdroid.registro.Interfaces.Communication;
 import com.sharpdroid.registro.R;
 
+import java.io.File;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
@@ -21,9 +31,13 @@ import butterknife.ButterKnife;
 
 public class CommunicationAdapter extends RecyclerView.Adapter<CommunicationAdapter.CommunicationHolder> {
     private final List<Communication> CVDataList;
+    private final Context mContext;
+    private final CoordinatorLayout mCoordinatorLayout;
 
-    public CommunicationAdapter(List<Communication> CVDataList) {
+    public CommunicationAdapter(Context mContext, CoordinatorLayout mCoordinatorLayout, List<Communication> CVDataList) {
         this.CVDataList = CVDataList;
+        this.mContext = mContext;
+        this.mCoordinatorLayout = mCoordinatorLayout;
     }
 
     public void addAll(Collection<Communication> list) {
@@ -58,6 +72,36 @@ public class CommunicationAdapter extends RecyclerView.Adapter<CommunicationAdap
             ViewHolder.Date.setText(datestring);
         }
         ViewHolder.Type.setText(communication.getType());
+
+        ViewHolder.mRelativeLayout.setOnClickListener(v -> {
+            Snackbar DownloadProgressSnak;
+            DownloadProgressSnak = Snackbar.make(mCoordinatorLayout, R.string.download_in_corso, Snackbar.LENGTH_INDEFINITE);
+            DownloadProgressSnak.show();
+
+            File file = new File(
+                    Environment.getExternalStoragePublicDirectory(
+                            Environment.DIRECTORY_DOWNLOADS).toString() +
+                            File.separator +
+                            "Registro Elettronico" +
+                            File.separator +
+                            communication.getId() + ".pdf");
+
+            String url = new RESTFulAPI().COMMUNICATION_DOWNLOAD_URL(communication.getId());
+
+            Ion.with(mContext)
+                    .load(url)
+                    .write(file)
+                    .withResponse()
+                    .setCallback((e, result) -> {
+                        if (result.getHeaders().code() == 200) {
+                            Intent intent = new Intent(Intent.ACTION_VIEW);
+                            intent.setDataAndType(FileProvider.getUriForFile(mContext, "com.sharpdroid.registro.fileprovider", file), "application/pdf");
+                            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_ACTIVITY_NEW_TASK);
+                            mContext.startActivity(intent);
+                            DownloadProgressSnak.dismiss();
+                        }
+                    });
+        });
     }
 
     @Override
@@ -66,6 +110,8 @@ public class CommunicationAdapter extends RecyclerView.Adapter<CommunicationAdap
     }
 
     class CommunicationHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.relative_layout)
+        RelativeLayout mRelativeLayout;
         @BindView(R.id.title)
         TextView Title;
         @BindView(R.id.date)
