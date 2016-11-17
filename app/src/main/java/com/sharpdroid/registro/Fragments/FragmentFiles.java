785df105525a225096dba62303111fab84e4d6af
Marco <bortolanmarco@gmail.com>
2016-11-17 20:48:14 +0100

    FragmentFile

---
+++ app/src/main/java/com/sharpdroid/registro/Fragments/FragmentFiles.java
@@ -0,0 +1,25 @@
+package com.sharpdroid.registro.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sharpdroid.registro.R;
+
+public class FragmentFiles extends Fragment {
+
+
+    public FragmentFiles() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_files, container, false);
+    }
+
+}
