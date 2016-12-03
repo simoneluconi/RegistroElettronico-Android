d018ff75226024cc20f9e638fe7f5e079695f660
Marco <bortolanmarco@gmail.com>
2016-12-03 21:49:38 +0100

    create MarkSubjectDetailActivity + todo

---
+++ app/src/main/java/com/sharpdroid/registro/Activities/MarkSubjectDetailActivity.java
@@ -0,0 +1,22 @@
+package com.sharpdroid.registro.Activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sharpdroid.registro.R;
+
+// TODO: 03/12/2016 Dettagli (nome, aula, prof, ora, note, colore)
+// TODO: 03/12/2016 Media (scritto, orale, totale)
+// TODO: 03/12/2016 Obiettivo
+// TODO: 03/12/2016 Orario settimanale (in quali giorni)
+// TODO: 03/12/2016 Verifiche prossime
+// TODO: 03/12/2016 Voti recenti
+
+public class MarkSubjectDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mark_subject_detail);
+    }
+}
