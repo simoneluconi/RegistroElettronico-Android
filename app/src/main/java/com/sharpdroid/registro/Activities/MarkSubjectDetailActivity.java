6f3b4ec5e37605edcc0cb1b7de8b8a3cb3cf67d8
Marco <bortolanmarco@gmail.com>
2016-12-03 22:58:51 +0100

    refactor

--- app/src/main/java/com/sharpdroid/registro/Activities/MarkSubjectDetailActivity.java
+++ app/src/main/java/com/sharpdroid/registro/Activities/MarkSubjectDetailActivity.java
@@ -1,19 +1,15 @@
 package com.sharpdroid.registro.Activities;
 
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
-import com.sharpdroid.registro.Interfaces.Media;
 import com.sharpdroid.registro.R;
 import com.sharpdroid.registro.Views.OverallView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
-import static com.sharpdroid.registro.Utils.Metodi.getMediaColor;
 
 // TODO: 03/12/2016 Dettagli (nome, aula, prof, ora, note, colore)
 // TODO: 03/12/2016 Media (scritto, orale, totale)
@@ -42,6 +38,6 @@ public class MarkSubjectDetailActivity extends AppCompatActivity {
 
     void setOverall() {
         overallView.setMedia("5.5");
-        overallView.setProgress(0.8f, ContextCompat.getColor(this,R.color.bluematerial));
+        overallView.setProgress(0.8f, ContextCompat.getColor(this, R.color.bluematerial));
     }
 }
