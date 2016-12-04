f38ba4f436d41c36d8678ac689231443b1e0d6e3
Luca Stefani <luca.stefani.ge1@gmail.com>
2016-12-04 19:24:31 +0100

    Improve MarkSubjectDetail

--- app/src/main/java/com/sharpdroid/registro/Activities/MarkSubjectDetailActivity.java
+++ app/src/main/java/com/sharpdroid/registro/Activities/MarkSubjectDetailActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import com.sharpdroid.registro.R;
 import com.sharpdroid.registro.Views.OverallView;
@@ -30,10 +31,27 @@ public class MarkSubjectDetailActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        // toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // add back arrow to toolbar
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+
         setOverall();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // handle arrow click here
+        if (item.getItemId() == android.R.id.home) {
+            finish(); // close this activity and return to preview activity (if there is any)
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     void setOverall() {
