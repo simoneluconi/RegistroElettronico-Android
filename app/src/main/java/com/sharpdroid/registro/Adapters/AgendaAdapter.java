5825c0e7b96f454a02ed4b12790f7baed9b938a7
Davide Della Giustina <dgdav99@gmail.com>
2016-12-10 22:54:30 +0100

    Added agenda section

---
+++ app/src/main/java/com/sharpdroid/registro/Adapters/AgendaAdapter.java
@@ -0,0 +1,163 @@
+package com.sharpdroid.registro.Adapters;
+
+import android.content.ActivityNotFoundException;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Environment;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.FileProvider;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.sharpdroid.registro.API.SpiaggiariApiClient;
+import com.sharpdroid.registro.Interfaces.Communication;
+import com.sharpdroid.registro.R;
+import com.vstechlab.easyfonts.EasyFonts;
+
+import java.io.File;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.sharpdroid.registro.Utils.Metodi.setTypeface;
+import static com.sharpdroid.registro.Utils.Metodi.writeResponseBodyToDisk;
+
+public class AgendaAdapter extends RecyclerView.Adapter<AgendaAdapter.CommunicationHolder> {
+    private final List<Communication> CVDataList;
+    private final Context mContext;
+    private final CoordinatorLayout mCoordinatorLayout;
+
+    public AgendaAdapter(Context mContext, CoordinatorLayout mCoordinatorLayout, List<Communication> CVDataList) {
+        this.CVDataList = CVDataList;
+        this.mContext = mContext;
+        this.mCoordinatorLayout = mCoordinatorLayout;
+    }
+
+    public void addAll(Collection<Communication> list) {
+        CVDataList.addAll(list);
+        notifyDataSetChanged();
+    }
+
+    public void clear() {
+        CVDataList.clear();
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public CommunicationHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.adapter_communications, parent, false);
+        return new CommunicationHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(CommunicationHolder ViewHolder, int i) {
+        final Communication communication = CVDataList.get(ViewHolder.getAdapterPosition());
+        ViewHolder.Title.setText(communication.getTitle().trim());
+        String datestring = communication.getDate().split("T")[0];
+        try {
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd", Locale.ITALIAN);
+            Date convertedCommitDate = formatter.parse(datestring);
+            //formatter = new SimpleDateFormat("dd/MM/YYYY", Locale.ITALIAN);
+            formatter = new SimpleDateFormat("d MMMM", Locale.ITALIAN);
+            ViewHolder.Date.setText(formatter.format(convertedCommitDate));
+        } catch (ParseException e) {
+            ViewHolder.Date.setText(datestring);
+        }
+        ViewHolder.Type.setText(communication.getType());
+
+        // TODO: 18/11/2016 aggiungere listener solo con allegati presenti
+        ViewHolder.mRelativeLayout.setOnClickListener(v -> {
+            Snackbar DownloadProgressSnak = Snackbar.make(mCoordinatorLayout, R.string.download_in_corso, Snackbar.LENGTH_INDEFINITE);
+
+            File dir = new File(
+                    Environment.getExternalStoragePublicDirectory(
+                            Environment.DIRECTORY_DOWNLOADS).toString() +
+                            File.separator +
+                            "Registro Elettronico");
+
+            File file = new File(dir +
+                    File.separator +
+                    communication.getId() + ".pdf");
+
+            communication.getId();
+
+            if (!file.exists()) {
+                DownloadProgressSnak.show();
+
+                if (!dir.exists()) dir.mkdir();
+                new SpiaggiariApiClient(mContext).mService.getcommunicationDownload(communication.getId()).enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        if (response.isSuccessful()) {
+                            if (writeResponseBodyToDisk(response.body(), file))
+                                openpdf(file, DownloadProgressSnak);
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+                        DownloadProgressSnak.setText(mContext.getResources().getString(R.string.download_fallito, t.getCause()));
+                        DownloadProgressSnak.setDuration(Snackbar.LENGTH_SHORT).show();
+                    }
+                });
+            } else {
+                openpdf(file, DownloadProgressSnak);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return CVDataList.size();
+    }
+
+    private void openpdf(File file, Snackbar DownloadProgressSnak) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setDataAndType(FileProvider.getUriForFile(mContext, "com.sharpdroid.registro.fileprovider", file), "application/pdf");
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+        DownloadProgressSnak.setText(R.string.click_to_open);
+        DownloadProgressSnak.setAction(R.string.open, v -> {
+            try {
+                mContext.startActivity(intent);
+            } catch (ActivityNotFoundException e) {
+                Snackbar.make(mCoordinatorLayout, mContext.getResources().getString(R.string.missing_pdf_app), Snackbar.LENGTH_SHORT).show();
+            }
+        });
+        DownloadProgressSnak.show();
+    }
+
+    class CommunicationHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.relative_layout)
+        RelativeLayout mRelativeLayout;
+        @BindView(R.id.title)
+        TextView Title;
+        @BindView(R.id.date)
+        TextView Date;
+        @BindView(R.id.type)
+        TextView Type;
+
+        CommunicationHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            setTypeface(EasyFonts.robotoRegular(mContext), Title, Type);
+            setTypeface(EasyFonts.robotoLight(mContext), Date);
+        }
+    }
+}
---
+++ app/src/main/java/com/sharpdroid/registro/Fragments/FragmentAgenda.java
@@ -0,0 +1,159 @@
+package com.sharpdroid.registro.Fragments;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.CoordinatorLayout;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sharpdroid.registro.API.SpiaggiariApiClient;
+import com.sharpdroid.registro.Adapters.CommunicationAdapter;
+import com.sharpdroid.registro.Interfaces.Communication;
+import com.sharpdroid.registro.R;
+import com.sharpdroid.registro.Tasks.CacheListTask;
+import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.StreamCorruptedException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import static com.sharpdroid.registro.Utils.Metodi.isNetworkAvailable;
+
+public class FragmentAgenda extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+    final private String TAG = FragmentAgenda.class.getSimpleName();
+
+    @BindView(R.id.coordinator_layout)
+    CoordinatorLayout mCoordinatorLayout;
+    @BindView(R.id.swiperefresh)
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
+    private CommunicationAdapter mRVAdapter;
+    private Context mContext;
+
+    public FragmentAgenda() {
+
+    }
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        mContext = getContext();
+        View layout = inflater.inflate(R.layout.fragment_communications, container, false);
+
+        ButterKnife.bind(this, layout);
+
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        mSwipeRefreshLayout.setColorSchemeResources(
+                R.color.bluematerial,
+                R.color.redmaterial,
+                R.color.greenmaterial,
+                R.color.orangematerial);
+
+        RecyclerView mRecyclerView = (RecyclerView) layout.findViewById(R.id.recycler);
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
+        mRecyclerView.addItemDecoration(new HorizontalDividerItemDecoration.Builder(mContext).size(1).build());
+        mRecyclerView.setItemAnimator(null);
+
+        mRVAdapter = new CommunicationAdapter(mContext, mCoordinatorLayout, new CopyOnWriteArrayList<>());
+        mRecyclerView.setAdapter(mRVAdapter);
+
+        bindCommunicationsCache();
+
+        UpdateCommunications();
+
+        return layout;
+    }
+
+    private void addCommunications(List<Communication> communications, boolean docache) {
+        if (!communications.isEmpty()) {
+            mRVAdapter.clear();
+            mRVAdapter.addAll(communications);
+
+            if (docache) {
+                // Update cache
+                new CacheListTask(mContext.getCacheDir(), TAG).execute((List) communications);
+            }
+        }
+    }
+
+    private void bindCommunicationsCache() {
+        ObjectInputStream objectInputStream = null;
+        try {
+            FileInputStream fileInputStream = new FileInputStream(new File(mContext.getCacheDir(), TAG));
+            objectInputStream = new ObjectInputStream(fileInputStream);
+            List<Communication> cachedData = new LinkedList<>();
+            Communication temp;
+            while ((temp = (Communication) objectInputStream.readObject()) != null) {
+                cachedData.add(temp);
+            }
+            addCommunications(cachedData, false);
+            Log.d(TAG, "Restored cache");
+        } catch (FileNotFoundException e) {
+            Log.w(TAG, "Cache not found.");
+        } catch (EOFException e) {
+            Log.e(TAG, "Error while reading cache! (EOF) ");
+        } catch (StreamCorruptedException e) {
+            Log.e(TAG, "Corrupted cache!");
+        } catch (IOException e) {
+            Log.e(TAG, "Error while reading cache!");
+        } catch (ClassCastException | ClassNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            if (objectInputStream != null) {
+                try {
+                    objectInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void onRefresh() {
+        UpdateCommunications();
+    }
+
+    private void UpdateCommunications() {
+        if (isNetworkAvailable(mContext)) {
+            mSwipeRefreshLayout.setRefreshing(true);
+            new SpiaggiariApiClient(mContext).mService.getCommunications().enqueue(new Callback<List<Communication>>() {
+                @Override
+                public void onResponse(Call<List<Communication>> call, Response<List<Communication>> response) {
+                    if (response.isSuccessful()) {
+                        addCommunications(response.body(), true);
+                        mSwipeRefreshLayout.setRefreshing(false);
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<List<Communication>> call, Throwable t) {
+
+                }
+            });
+        } else {
+            Snackbar.make(mCoordinatorLayout, R.string.nointernet, Snackbar.LENGTH_LONG).show();
+            mSwipeRefreshLayout.setRefreshing(false);
+        }
+    }
+}
