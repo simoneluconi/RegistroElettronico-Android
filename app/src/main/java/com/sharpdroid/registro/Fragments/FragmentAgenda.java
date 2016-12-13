f191a8e204fc188f87ac729048364ee4d917d1e8
Marco <bortolanmarco@gmail.com>
2016-12-13 22:01:32 +0100

    cleanup

--- app/src/main/java/com/sharpdroid/registro/Fragments/FragmentAgenda.java
+++ app/src/main/java/com/sharpdroid/registro/Fragments/FragmentAgenda.java
@@ -2,51 +2,20 @@ package com.sharpdroid.registro.Fragments;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.design.widget.CoordinatorLayout;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
-import com.sharpdroid.registro.API.SpiaggiariApiClient;
-import com.sharpdroid.registro.Adapters.CommunicationAdapter;
-import com.sharpdroid.registro.Interfaces.Communication;
 import com.sharpdroid.registro.R;
-import com.sharpdroid.registro.Tasks.CacheListTask;
-import com.yqritc.recyclerviewflexibledivider.HorizontalDividerItemDecoration;
 
-import java.io.EOFException;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.StreamCorruptedException;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-
-import butterknife.BindView;
 import butterknife.ButterKnife;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 
-import static com.sharpdroid.registro.Utils.Metodi.isNetworkAvailable;
-
-public class FragmentAgenda extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+public class FragmentAgenda extends Fragment {
     final private String TAG = FragmentAgenda.class.getSimpleName();
 
-    @BindView(R.id.coordinator_layout)
-    CoordinatorLayout mCoordinatorLayout;
-    @BindView(R.id.swiperefresh)
-    SwipeRefreshLayout mSwipeRefreshLayout;
 
-    private CommunicationAdapter mRVAdapter;
     private Context mContext;
 
     public FragmentAgenda() {
@@ -57,97 +26,11 @@ public class FragmentAgenda extends Fragment implements SwipeRefreshLayout.OnRef
     public View onCreateView(final LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         mContext = getContext();
-        View layout = inflater.inflate(R.layout.fragment_communications, container, false);
+        View layout = inflater.inflate(R.layout.fragment_calendar, container, false);
 
         ButterKnife.bind(this, layout);
-
-        mSwipeRefreshLayout.setOnRefreshListener(this);
-        mSwipeRefreshLayout.setColorSchemeResources(
-                R.color.bluematerial,
-                R.color.redmaterial,
-                R.color.greenmaterial,
-                R.color.orangematerial);
-
-        RecyclerView mRecyclerView = (RecyclerView) layout.findViewById(R.id.recycler);
-        mRecyclerView.setHasFixedSize(true);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(mContext));
-        mRecyclerView.addItemDecoration(new HorizontalDividerItemDecoration.Builder(mContext).size(1).build());
-        mRecyclerView.setItemAnimator(null);
-
-        mRVAdapter = new CommunicationAdapter(mContext, mCoordinatorLayout, new CopyOnWriteArrayList<>());
-        mRecyclerView.setAdapter(mRVAdapter);
-
-        bindCommunicationsCache();
-
-        UpdateCommunications();
 
         return layout;
     }
 
-    private void addCommunications(List<Communication> communications, boolean docache) {
-        if (!communications.isEmpty()) {
-            mRVAdapter.clear();
-            mRVAdapter.addAll(communications);
-
-            if (docache) {
-                // Update cache
-                new CacheListTask(mContext.getCacheDir(), TAG).execute((List) communications);
-            }
-        }
-    }
-
-    private void bindCommunicationsCache() {
-        ObjectInputStream objectInputStream = null;
-        try {
-            FileInputStream fileInputStream = new FileInputStream(new File(mContext.getCacheDir(), TAG));
-            objectInputStream = new ObjectInputStream(fileInputStream);
-            List<Communication> cachedData = new LinkedList<>();
-            Communication temp;
-            while ((temp = (Communication) objectInputStream.readObject()) != null) {
-                cachedData.add(temp);
-            }
-            addCommunications(cachedData, false);
-            Log.d(TAG, "Restored cache");
-        } catch (FileNotFoundException e) {
-            Log.w(TAG, "Cache not found.");
-        } catch (EOFException e) {
-            Log.e(TAG, "Error while reading cache! (EOF) ");
-        } catch (StreamCorruptedException e) {
-            Log.e(TAG, "Corrupted cache!");
-        } catch (IOException e) {
-            Log.e(TAG, "Error while reading cache!");
-        } catch (ClassCastException | ClassNotFoundException e) {
-            e.printStackTrace();
-        } finally {
-            if (objectInputStream != null) {
-                try {
-                    objectInputStream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
-    public void onRefresh() {
-        UpdateCommunications();
-    }
-
-    private void UpdateCommunications() {
-        if (isNetworkAvailable(mContext)) {
-            mSwipeRefreshLayout.setRefreshing(true);
-            new SpiaggiariApiClient(mContext).mService.getCommunications()
-                    .subscribeOn(Schedulers.newThread())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(communications -> {
-                        addCommunications(communications, true);
-                        mSwipeRefreshLayout.setRefreshing(false);
-                    }, error -> {
-                        mSwipeRefreshLayout.setRefreshing(false);
-                    });
-        } else {
-            Snackbar.make(mCoordinatorLayout, R.string.nointernet, Snackbar.LENGTH_LONG).show();
-            mSwipeRefreshLayout.setRefreshing(false);
-        }
-    }
 }
