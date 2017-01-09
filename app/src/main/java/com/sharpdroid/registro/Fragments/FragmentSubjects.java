11c29adcb99d961b6fb4d6391f2d11629b811f14
Marco <bortolanmarco@gmail.com>
2017-01-09 14:58:44 +0100

    Lessons Fragments

---
+++ app/src/main/java/com/sharpdroid/registro/Fragments/FragmentSubjects.java
@@ -0,0 +1,27 @@
+package com.sharpdroid.registro.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.sharpdroid.registro.R;
+
+public class FragmentSubjects extends Fragment {
+
+
+    public FragmentSubjects() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_subjects, container, false);
+    }
+
+}
