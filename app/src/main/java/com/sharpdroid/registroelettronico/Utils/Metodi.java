794f0d0a65f9b670d47cdfc78bf41bf813688649
Simone Luconi <luconi.simone@gmail.com>
2017-01-21 15:28:23 +0100

    cleanup

--- app/src/main/java/com/sharpdroid/registroelettronico/Utils/Metodi.java
+++ app/src/main/java/com/sharpdroid/registroelettronico/Utils/Metodi.java
@@ -14,8 +14,6 @@ import com.sharpdroid.registroelettronico.Interfaces.API.Absence;
 import com.sharpdroid.registroelettronico.Interfaces.API.Absences;
 import com.sharpdroid.registroelettronico.Interfaces.API.Delay;
 import com.sharpdroid.registroelettronico.Interfaces.API.Exit;
-import com.sharpdroid.registroelettronico.Interfaces.API.FileTeacher;
-import com.sharpdroid.registroelettronico.Interfaces.API.Folder;
 import com.sharpdroid.registroelettronico.Interfaces.API.Mark;
 import com.sharpdroid.registroelettronico.Interfaces.API.MarkSubject;
 import com.sharpdroid.registroelettronico.Interfaces.Client.AbsenceEntry;
@@ -38,7 +36,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedList;
@@ -135,13 +132,11 @@ public class Metodi {
 
         // Sorting the list based on values
 
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-            public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
-                if (order) {
-                    return o1.getValue().compareTo(o2.getValue());
-                } else {
-                    return o2.getValue().compareTo(o1.getValue());
-                }
+        Collections.sort(list, (Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) -> {
+            if (order) {
+                return o1.getValue().compareTo(o2.getValue());
+            } else {
+                return o2.getValue().compareTo(o1.getValue());
             }
         });
 
@@ -242,18 +237,6 @@ public class Metodi {
             convert.add((com.sharpdroid.registroelettronico.Interfaces.API.Event) e.getData());
         }
         return convert;
-    }
-
-    public static List<Integer> getListLayouts(List<FileTeacher> data) {
-        List<Integer> list = new ArrayList<>();
-
-        for (FileTeacher fileTeacher : data) {
-            list.add(R.layout.adapter_file_teacher);
-            for (Folder ignored : fileTeacher.getFolders()) {
-                list.add(R.layout.adapter_folder);
-            }
-        }
-        return list;
     }
 
     public static String toLowerCase(String s) {
