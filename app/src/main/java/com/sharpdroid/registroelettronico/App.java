426b10ede2a283141337b0f2b27cfc554794e09f
Simone Luconi <luconi.simone@gmail.com>
2017-01-28 21:54:40 +0100

    fixed drawable crash on pre lollipop devices

---
+++ app/src/main/java/com/sharpdroid/registroelettronico/App.java
@@ -0,0 +1,12 @@
+package com.sharpdroid.registroelettronico;
+
+
+import android.app.Application;
+import android.support.v7.app.AppCompatDelegate;
+
+public class App extends Application {
+
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
+}
