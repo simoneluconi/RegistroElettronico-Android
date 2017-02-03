5e4a0e003b667b2db6c971d9ecbeda86fdb37502
Luca Stefani <luca.stefani.ge1@gmail.com>
2017-02-03 23:07:39 +0100

    Weird stuff happens

--- app/src/main/java/com/sharpdroid/registroelettronico/Fragments/FragmentAgenda.java
+++ app/src/main/java/com/sharpdroid/registroelettronico/Fragments/FragmentAgenda.java
@@ -33,7 +33,6 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 
-import static com.sharpdroid.registroelettronico.R.id.fab;
 import static com.sharpdroid.registroelettronico.Utils.Metodi.convertEvents;
 
 // TODO: 19/01/2017 Aggiungere eventi all'agenda 
@@ -48,7 +47,7 @@ public class FragmentAgenda extends Fragment implements CompactCalendarView.Comp
     View place_holder;
     @BindView(R.id.coordinator_layout)
     CoordinatorLayout mCoordinatorLayout;
-    @BindView(fab)
+    @BindView(R.id.fab)
     FloatingActionButton mFloatingActionButton;
 
     private CompactCalendarView mCompactCalendarView;
