d0d66a43bc0398a56b5bbdc3be2d39773c53998b
Marco <bortolanmarco@gmail.com>
2017-05-02 22:35:39 +0200

    ItemOffsetDecoration

---
+++ app/src/main/java/com/sharpdroid/registroelettronico/Utils/ItemOffsetDecoration.kt
@@ -0,0 +1,18 @@
+package com.sharpdroid.registroelettronico.Utils
+
+import android.content.Context
+import android.graphics.Rect
+import android.support.annotation.DimenRes
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+class ItemOffsetDecoration private constructor(private val mItemOffset: Int) : RecyclerView.ItemDecoration() {
+
+    constructor(context: Context, @DimenRes itemOffsetId: Int) : this(context.resources.getDimensionPixelSize(itemOffsetId))
+
+    override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView,
+                                state: RecyclerView.State?) {
+        super.getItemOffsets(outRect, view, parent, state)
+        outRect.set(mItemOffset, mItemOffset, mItemOffset, mItemOffset)
+    }
+}
