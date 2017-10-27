71e366a310b6a1d384dbb034c04a632ced9f0d92
Luca Stefani <luca.stefani.ge1@gmail.com>
2017-10-27 23:40:51 +0200

    Info -> Kotlin

--- app/src/main/java/com/sharpdroid/registroelettronico/Info.java
+++
@@ -1,6 +0,0 @@
-package com.sharpdroid.registroelettronico;
-
-public class Info {
-    public static final String ACCOUNT = "currentProfile";
-    public static final String API_URL = "https://web.spaggiari.eu/rest/v1/";
-}
---
+++ app/src/main/java/com/sharpdroid/registroelettronico/Info.kt
@@ -0,0 +1,6 @@
+package com.sharpdroid.registroelettronico
+
+object Info {
+    val ACCOUNT = "currentProfile"
+    val API_URL = "https://web.spaggiari.eu/rest/v1/"
+}
