e918954a9f03f40caab50c8e19bf2d6a97faa50f
Luca Stefani <luca.stefani.ge1@gmail.com>
2017-10-28 23:05:23 +0200

    Move fabric init to App

--- app/src/main/java/com/sharpdroid/registroelettronico/App.kt
+++ app/src/main/java/com/sharpdroid/registroelettronico/App.kt
@@ -4,9 +4,12 @@ import android.app.Application
 import android.content.Intent
 import android.support.v7.app.AppCompatDelegate
 import android.util.Log
+import com.crashlytics.android.Crashlytics
+import com.crashlytics.android.answers.Answers
 import com.google.android.gms.security.ProviderInstaller
 import com.google.firebase.FirebaseApp
 import com.orm.SugarContext
+import io.fabric.sdk.android.Fabric
 
 class App : Application() {
 
@@ -17,6 +20,17 @@ class App : Application() {
         upgradeSecurityProvider()
         SugarContext.init(this)
         FirebaseApp.initializeApp(this)
+
+        // Install a hook to Crashlytics and Answers (only in production releases)
+        if (!BuildConfig.DEBUG) {
+            try {
+                Fabric.with(this, Crashlytics(), Answers())
+            } catch (e: Error) {
+                // Ignore any fabric exception by miss-configuration
+                Log.e(TAG, "Cannot configure Fabric", e)
+            }
+
+        }
     }
 
     private fun upgradeSecurityProvider() {
