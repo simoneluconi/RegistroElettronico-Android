86b26b4d94d84e7d4bda29a3ca4fbfdb571f8849
Quix0tic <bortolanmarco@gmail.com>
2017-11-04 20:29:24 +0100

    Update didattica on profile change

--- app/src/main/java/com/sharpdroid/registroelettronico/database/viewModels/DidatticaViewModel.kt
+++ app/src/main/java/com/sharpdroid/registroelettronico/database/viewModels/DidatticaViewModel.kt
@@ -10,11 +10,13 @@ import com.sharpdroid.registroelettronico.database.room.DatabaseHelper
 class DidatticaViewModel : ViewModel() {
     private var didattica: LiveData<List<TeacherDidacticPOJO>>? = null
     var selectedFolder = MutableLiveData<FolderPOJO>()
+    var profile = MutableLiveData<Long>()
 
     fun getDidattica(profile: Long): LiveData<List<TeacherDidacticPOJO>> {
-        if (didattica == null) {
+        if (didattica == null || this.profile.value != profile) {
             didattica = DatabaseHelper.database.foldersDao().getDidattica(profile)
         }
+        this.profile.value = profile
         return didattica ?: throw NullPointerException("Didattica's livedata not yet initialized")
     }
 }
