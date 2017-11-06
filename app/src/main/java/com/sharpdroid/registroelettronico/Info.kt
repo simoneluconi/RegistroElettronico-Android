82889b4f6628f9420bd3e3791e910f95ee2109f2
Quix0tic <bortolanmarco@gmail.com>
2017-11-06 19:23:13 +0100

    2->10

--- app/src/main/java/com/sharpdroid/registroelettronico/Info.kt
+++ app/src/main/java/com/sharpdroid/registroelettronico/Info.kt
@@ -1,6 +1,10 @@
 package com.sharpdroid.registroelettronico
 
+import java.util.concurrent.atomic.AtomicInteger
+
 object Info {
     val ACCOUNT = "currentProfile"
     val API_URL = "https://web.spaggiari.eu/rest/v1/"
+
+    val notificationId = AtomicInteger(0)
 }
