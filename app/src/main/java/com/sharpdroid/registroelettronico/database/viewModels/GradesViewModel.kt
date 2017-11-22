b510c9b48ca18b228e75731aaaf3360aca90662c
Luca Stefani <luca.stefani.ge1@gmail.com>
2017-11-22 16:56:24 +0100

    Restore medie order default value from pref

--- app/src/main/java/com/sharpdroid/registroelettronico/database/viewModels/GradesViewModel.kt
+++ app/src/main/java/com/sharpdroid/registroelettronico/database/viewModels/GradesViewModel.kt
@@ -1,12 +1,14 @@
 package com.sharpdroid.registroelettronico.database.viewModels
 
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
-import android.arch.lifecycle.ViewModel
+import android.preference.PreferenceManager
 import com.sharpdroid.registroelettronico.database.entities.Average
 import com.sharpdroid.registroelettronico.database.room.DatabaseHelper
 
-class GradesViewModel : ViewModel() {
+class GradesViewModel(app: Application) : AndroidViewModel(app) {
     private var firstPeriod: LiveData<List<Average>>? = null
     private var secondPeriod: LiveData<List<Average>>? = null
     private var allPeriods: LiveData<List<Average>>? = null
@@ -16,29 +18,31 @@ class GradesViewModel : ViewModel() {
     private var thirdProfile = 0L
     var selected = 0
 
-    fun getGrades(profile: Long, period: Int): LiveData<List<Average>> {
-        return when (period) {
-            1 -> {
-                if (firstPeriod == null || profile != this.firstProfile) {
-                    firstPeriod = DatabaseHelper.database.gradesDao().getAverages(profile, 1)
-                    this.firstProfile = profile
-                }
-                firstPeriod ?: throw NullPointerException("firstPeriod livedata not yet initialized")
+    init {
+        order.value = PreferenceManager.getDefaultSharedPreferences(getApplication()).getString("order", "")
+    }
+
+    fun getGrades(profile: Long, period: Int): LiveData<List<Average>> = when (period) {
+        1 -> {
+            if (firstPeriod == null || profile != this.firstProfile) {
+                firstPeriod = DatabaseHelper.database.gradesDao().getAverages(profile, 1)
+                this.firstProfile = profile
             }
-            3 -> {
-                if (secondPeriod == null || profile != this.secondProfile) {
-                    secondPeriod = DatabaseHelper.database.gradesDao().getAverages(profile, 3)
-                    this.secondProfile = profile
-                }
-                secondPeriod ?: throw NullPointerException("secondPeriod livedata not yet initialized")
+            firstPeriod ?: throw NullPointerException("firstPeriod livedata not yet initialized")
+        }
+        3 -> {
+            if (secondPeriod == null || profile != this.secondProfile) {
+                secondPeriod = DatabaseHelper.database.gradesDao().getAverages(profile, 3)
+                this.secondProfile = profile
             }
-            else -> {
-                if (allPeriods == null || profile != this.thirdProfile) {
-                    allPeriods = DatabaseHelper.database.gradesDao().getAllAverages(profile)
-                    this.thirdProfile = profile
-                }
-                allPeriods ?: throw NullPointerException("allPeriods livedata not yet initialized")
+            secondPeriod ?: throw NullPointerException("secondPeriod livedata not yet initialized")
+        }
+        else -> {
+            if (allPeriods == null || profile != this.thirdProfile) {
+                allPeriods = DatabaseHelper.database.gradesDao().getAllAverages(profile)
+                this.thirdProfile = profile
             }
+            allPeriods ?: throw NullPointerException("allPeriods livedata not yet initialized")
         }
     }
 
