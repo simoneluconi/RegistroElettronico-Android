30d78b1ff7a4d25781be08a63ff168b1ce5f846f
Luca Stefani <luca.stefani.ge1@gmail.com>
2017-11-26 13:16:24 +0100

    Remove uneeded isDatabaseCreated

--- app/src/main/java/com/sharpdroid/registroelettronico/database/room/DatabaseHelper.kt
+++ app/src/main/java/com/sharpdroid/registroelettronico/database/room/DatabaseHelper.kt
@@ -1,22 +1,16 @@
 package com.sharpdroid.registroelettronico.database.room
 
-import android.arch.lifecycle.MutableLiveData
 import android.arch.persistence.db.SupportSQLiteDatabase
 import android.arch.persistence.room.Room
 import android.arch.persistence.room.migration.Migration
 import android.content.Context
-import io.reactivex.Completable
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.atomic.AtomicBoolean
 
 object DatabaseHelper {
 
-    private val isDatabaseCreated = MutableLiveData<Boolean>()
-
     lateinit var database: RoomDB
 
-    private val isInitializing = AtomicBoolean(true)
+    private val initialized = AtomicBoolean(false)
 
     private val MIGRATION_4_5 = object : Migration(4, 5) {
         override fun migrate(database: SupportSQLiteDatabase) {
@@ -103,23 +97,12 @@ object DatabaseHelper {
     }
 
     fun createDb(context: Context) {
-        if (isInitializing.compareAndSet(true, false).not()) {
-            return
-        }
-
-        isDatabaseCreated.value = false
-
-        Completable.fromAction({
+        if (initialized.compareAndSet(false, true)) {
             database = Room.databaseBuilder(context, RoomDB::class.java, "registro.db")
                     .allowMainThreadQueries()
                     .fallbackToDestructiveMigration()
                     .addMigrations(MIGRATION_4_5, MIGRATION_5_6)
                     .build()
-        })
-                .subscribeOn(Schedulers.computation())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe {
-                    isDatabaseCreated.value = true
-                }
+        }
     }
 }
