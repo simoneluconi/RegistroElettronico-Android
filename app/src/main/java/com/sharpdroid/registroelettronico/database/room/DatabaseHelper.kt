d128f4b83c532ee913d167da2ef2102b78d6cfb7
Quix0tic <bortolanmarco@gmail.com>
2017-11-26 02:09:03 +0100

    Fallback to destructive migrations for old versions

--- app/src/main/java/com/sharpdroid/registroelettronico/database/room/DatabaseHelper.kt
+++ app/src/main/java/com/sharpdroid/registroelettronico/database/room/DatabaseHelper.kt
@@ -28,28 +28,22 @@ object DatabaseHelper {
         Completable.fromAction({
             database = Room.databaseBuilder(context, RoomDB::class.java, "registro.db")
                     .allowMainThreadQueries()
-                    .addMigrations(object : Migration(1, 4) {
-                        override fun migrate(database: SupportSQLiteDatabase) {
-                            with(database) {
-                                execSQL("DROP TABLE IF EXISTS PROFILE")
-                            }
-                        }
-                    })
+                    .fallbackToDestructiveMigration()
                     .addMigrations(object : Migration(4, 5) {
                         override fun migrate(database: SupportSQLiteDatabase) {
                             with(database) {
-                                execSQL("DROP TABLE ABSENCE")
-                                execSQL("DROP TABLE COMMUNICATION")
-                                execSQL("DROP TABLE FILE")
-                                execSQL("DROP TABLE FOLDER")
-                                execSQL("DROP TABLE GRADE")
-                                execSQL("DROP TABLE LESSON")
-                                execSQL("DROP TABLE NOTE")
-                                execSQL("DROP TABLE PERIOD")
-                                execSQL("DROP TABLE REMOTE_AGENDA")
-                                execSQL("DROP TABLE SUBJECT")
-                                execSQL("DROP TABLE SUBJECT_TEACHER")
-                                execSQL("DROP TABLE TEACHER")
+                                execSQL("DROP TABLE IF EXISTS ABSENCE")
+                                execSQL("DROP TABLE IF EXISTS COMMUNICATION")
+                                execSQL("DROP TABLE IF EXISTS FILE")
+                                execSQL("DROP TABLE IF EXISTS FOLDER")
+                                execSQL("DROP TABLE IF EXISTS GRADE")
+                                execSQL("DROP TABLE IF EXISTS LESSON")
+                                execSQL("DROP TABLE IF EXISTS NOTE")
+                                execSQL("DROP TABLE IF EXISTS PERIOD")
+                                execSQL("DROP TABLE IF EXISTS REMOTE_AGENDA")
+                                execSQL("DROP TABLE IF EXISTS SUBJECT")
+                                execSQL("DROP TABLE IF EXISTS SUBJECT_TEACHER")
+                                execSQL("DROP TABLE IF EXISTS TEACHER")
 
                                 execSQL("CREATE TABLE `ABSENCE` (`ID` INTEGER NOT NULL, `TYPE` TEXT NOT NULL, `DATE` INTEGER NOT NULL, `JUSTIFIED` INTEGER NOT NULL, `REASON_CODE` TEXT, `REASON_DESC` TEXT, `PROFILE` INTEGER NOT NULL, `H_POS` INTEGER NOT NULL, `VALUE` INTEGER NOT NULL, PRIMARY KEY(`ID`))")
                                 execSQL("CREATE TABLE `COMMUNICATION` (`ID` INTEGER NOT NULL, `DATE` INTEGER NOT NULL, `IS_READ` INTEGER NOT NULL, `EVT_CODE` TEXT NOT NULL, `MY_ID` INTEGER NOT NULL, `TITLE` TEXT NOT NULL, `CATEGORY` TEXT NOT NULL, `HAS_ATTACHMENT` INTEGER NOT NULL, `PROFILE` INTEGER NOT NULL, PRIMARY KEY(`ID`))")
@@ -71,38 +65,38 @@ object DatabaseHelper {
                                 execSQL("ALTER TABLE COMMUNICATION_INFO RENAME TO COMMUNICATION_INFO_OLD")
                                 execSQL("CREATE TABLE `COMMUNICATION_INFO` (`ID` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `TITLE` TEXT NOT NULL, `CONTENT` TEXT NOT NULL, `PATH` TEXT NOT NULL)")
                                 execSQL("INSERT INTO COMMUNICATION_INFO(`ID`, `TITLE`, `CONTENT`, `PATH`) SELECT `ID`, `TITLE`, `CONTENT`, `PATH` FROM COMMUNICATION_INFO_OLD")
-                                execSQL("DROP TABLE COMMUNICATION_INFO_OLD")
+                                execSQL("DROP TABLE IF EXISTS COMMUNICATION_INFO_OLD")
 
                                 execSQL("ALTER TABLE FILE_INFO RENAME TO FILE_INFO_OLD")
                                 execSQL("CREATE TABLE `FILE_INFO` (`ID` INTEGER NOT NULL, `PATH` TEXT NOT NULL, PRIMARY KEY(`ID`))")
                                 execSQL("INSERT INTO FILE_INFO (ID, PATH) SELECT ID, PATH FROM FILE_INFO_OLD")
-                                execSQL("DROP TABLE FILE_INFO_OLD")
+                                execSQL("DROP TABLE IF EXISTS FILE_INFO_OLD")
 
                                 execSQL("ALTER TABLE LOCAL_AGENDA RENAME TO LOCAL_AGENDA_OLD")
                                 execSQL("UPDATE LOCAL_AGENDA_OLD SET COMPLETEDDATE=0 WHERE COMPLETEDDATE IS NULL")
                                 execSQL("CREATE TABLE `LOCAL_AGENDA` (`ID` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `TITLE` TEXT NOT NULL, `CONTENT` TEXT NOT NULL, `TYPE` TEXT NOT NULL, `DAY` INTEGER NOT NULL, `SUBJECT` INTEGER NOT NULL, `TEACHER` INTEGER NOT NULL, `COMPLETED_DATE` INTEGER, `PROFILE` INTEGER NOT NULL, `ARCHIVED` INTEGER NOT NULL)")
                                 execSQL("INSERT INTO LOCAL_AGENDA(ID, TITLE, CONTENT, TYPE, DAY, SUBJECT, TEACHER, COMPLETED_DATE, PROFILE, ARCHIVED) SELECT ID, TITLE, CONTENT, TYPE, DAY, SUBJECT, TEACHER, COMPLETEDDATE as COMPLETED_DATE, PROFILE, ARCHIVED FROM LOCAL_AGENDA_OLD")
-                                execSQL("DROP TABLE LOCAL_AGENDA_OLD")
+                                execSQL("DROP TABLE IF EXISTS LOCAL_AGENDA_OLD")
 
                                 execSQL("ALTER TABLE LOCAL_GRADE RENAME TO LOCAL_GRADE_OLD")
                                 execSQL("CREATE TABLE `LOCAL_GRADE` (`ID` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `VALUE` REAL NOT NULL, `VALUE_NAME` TEXT NOT NULL, `SUBJECT` INTEGER NOT NULL, `PERIOD` INTEGER NOT NULL, `TYPE` TEXT NOT NULL, `PROFILE` INTEGER NOT NULL)")
                                 execSQL("INSERT INTO LOCAL_GRADE(ID, VALUE, VALUE_NAME, SUBJECT, PERIOD, TYPE, PROFILE) SELECT ID, VALUE, VALUENAME as VALUE_NAME, SUBJECT, PERIOD, TYPE, PROFILE FROM LOCAL_GRADE_OLD")
-                                execSQL("DROP TABLE LOCAL_GRADE_OLD")
+                                execSQL("DROP TABLE IF EXISTS LOCAL_GRADE_OLD")
 
                                 execSQL("ALTER TABLE PROFILE RENAME TO PROFILE_OLD")
                                 execSQL("CREATE TABLE `PROFILE` (`USERNAME` TEXT NOT NULL, `NAME` TEXT NOT NULL, `PASSWORD` TEXT NOT NULL, `CLASSE` TEXT NOT NULL, `ID` INTEGER NOT NULL, `TOKEN` TEXT NOT NULL, `EXPIRE` INTEGER NOT NULL, `IDENT` TEXT NOT NULL, `IS_MULTI` INTEGER NOT NULL, PRIMARY KEY(`ID`))")
                                 execSQL("INSERT INTO PROFILE(USERNAME, NAME, PASSWORD, CLASSE, ID, TOKEN, EXPIRE, IDENT, IS_MULTI) SELECT USERNAME, NAME, PASSWORD, CLASSE, ID, TOKEN, EXPIRE, IDENT, IS_MULTI FROM PROFILE_OLD")
-                                execSQL("DROP TABLE PROFILE_OLD")
+                                execSQL("DROP TABLE IF EXISTS PROFILE_OLD")
 
                                 execSQL("ALTER TABLE REMOTE_AGENDA_INFO RENAME TO REMOTE_AGENDA_INFO_OLD")
                                 execSQL("CREATE TABLE `REMOTE_AGENDA_INFO` (`ID` INTEGER NOT NULL, `COMPLETED` INTEGER NOT NULL, `ARCHIVED` INTEGER NOT NULL, `TEST` INTEGER NOT NULL, PRIMARY KEY(`ID`))")
                                 execSQL("INSERT INTO REMOTE_AGENDA_INFO(ID, COMPLETED, ARCHIVED, TEST) SELECT ID, COMPLETED, ARCHIVED, TEST FROM REMOTE_AGENDA_INFO_OLD")
-                                execSQL("DROP TABLE REMOTE_AGENDA_INFO_OLD")
+                                execSQL("DROP TABLE IF EXISTS REMOTE_AGENDA_INFO_OLD")
 
                                 execSQL("ALTER TABLE SUBJECT_INFO RENAME TO SUBJECT_INFO_OLD")
                                 execSQL("CREATE TABLE `SUBJECT_INFO` (`ID` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `TARGET` REAL NOT NULL, `DESCRIPTION` TEXT NOT NULL, `DETAILS` TEXT NOT NULL, `CLASSROOM` TEXT NOT NULL, `SUBJECT` INTEGER NOT NULL, `PROFILE` INTEGER NOT NULL)")
                                 execSQL("INSERT INTO SUBJECT_INFO(ID, TARGET, DESCRIPTION, DETAILS, CLASSROOM, SUBJECT, PROFILE) SELECT ID, TARGET, DESCRIPTION, DETAILS, CLASSROOM, SUBJECT, PROFILE FROM SUBJECT_INFO_OLD")
-                                execSQL("DROP TABLE SUBJECT_INFO_OLD")
+                                execSQL("DROP TABLE IF EXISTS SUBJECT_INFO_OLD")
                             }
                         }
                     }, object : Migration(5, 6) {
@@ -112,7 +106,7 @@ object DatabaseHelper {
                                 execSQL("UPDATE LOCAL_AGENDA_OLD SET COMPLETED_DATE=0 WHERE COMPLETED_DATE IS NULL")
                                 execSQL("CREATE TABLE `LOCAL_AGENDA` (`ID` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `TITLE` TEXT NOT NULL, `CONTENT` TEXT NOT NULL, `TYPE` TEXT NOT NULL, `DAY` INTEGER NOT NULL, `SUBJECT` INTEGER NOT NULL, `TEACHER` INTEGER NOT NULL, `COMPLETED_DATE` INTEGER NOT NULL, `PROFILE` INTEGER NOT NULL, `ARCHIVED` INTEGER NOT NULL)")
                                 execSQL("INSERT INTO LOCAL_AGENDA(ID, TITLE, CONTENT, TYPE, DAY, SUBJECT, TEACHER, COMPLETED_DATE, PROFILE, ARCHIVED) SELECT ID, TITLE, CONTENT, TYPE, DAY, SUBJECT, TEACHER, COMPLETED_DATE, PROFILE, ARCHIVED FROM LOCAL_AGENDA_OLD")
-                                execSQL("DROP TABLE LOCAL_AGENDA_OLD")
+                                execSQL("DROP TABLE IF EXISTS LOCAL_AGENDA_OLD")
                             }
                         }
                     })
